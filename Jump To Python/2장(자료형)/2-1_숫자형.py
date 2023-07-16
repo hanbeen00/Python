6fed8e506ef61cb8f17a8abd16bbf17769e1aa03
hanbeen00 <76528601+hanbeen00@users.noreply.github.com>
2023-07-16 22:32:39 +0900

    Create 2-1_숫자형.py

---
+++ Jump To Python/2장(자료형)/2-1_숫자형.py
@@ -0,0 +1,33 @@
+a=122 #정수형
+print(a)
+
+a=1.12 #실수형(소수)
+print(a)
+
+a=4.24e10#실수형(지수)
+print(a)
+a=4.24e-10
+print(a)
+
+a=0o177 #8진수
+print(a)
+
+a=0x8ff #16진수
+print(a)
+
+a=1+2j #복소수
+print(a)
+print(a.real) #실수
+print(a.imag) #허수
+print(a.conjugate()) #켤레복소수
+print(abs(a)) #절댓값
+
+a=3
+b=4
+print(a+b)
+print(a/b)
+print(a//b) #나눈후 정수부분
+print(a**b) #제곱
+print(a%b) #나머지
+
+
