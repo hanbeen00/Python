3368223784c60118c5e609f761da58ec7eefed4c
hanbeen00 <76528601+hanbeen00@users.noreply.github.com>
2023-08-03 23:20:28 +0900

    Create 부품찾기(집합).py

---
+++ 이코테/이론/이진탐색/실습/부품찾기(집합).py
@@ -0,0 +1,20 @@
+n = int(input())
+array = set(map(int, input().split()))
+
+m = int(input())
+x= list(map(int, input().split()))
+
+for i in x:
+    if i in array:
+        print('yes', end=' ')
+
+    else:
+        print('no', end=' ')
+
+
+'''입력 예시
+5
+8 3 7 9 2
+3
+5 7 9
+'''
